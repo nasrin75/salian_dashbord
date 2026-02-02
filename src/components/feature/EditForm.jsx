e86b89563db70aa8ce129192b41d084ae29b7b1d
Nasrin Hasanpour <n75hasanpur@gmail.com>
2026-02-02 10:09:04 +0330

    Update EditForm.jsx

--- src/components/feature/EditForm.jsx
+++ src/components/feature/EditForm.jsx
@@ -1,4 +1,4 @@
-import { useState, useCallback } from 'react';
+import { useState, useCallback, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
@@ -6,6 +6,15 @@ import FormGroup from '@mui/material/FormGroup';
 import Grid from '@mui/material/Grid';
 import Stack from '@mui/material/Stack';
 import TextField from '@mui/material/TextField';
+import FormControl from '@mui/material/FormControl';
+import InputLabel from '@mui/material/InputLabel';
+import Select from '@mui/material/Select';
+import MenuItem from '@mui/material/MenuItem';
+import { EquipmentDetails, getEquipments } from '../../api/EquipmentApi';
+import { useParams } from 'react-router-dom';
+import { FeatureDetails } from '../../api/FeatureApi';
+import { toast } from 'react-toastify';
+import OutlinedInput from '@mui/material/OutlinedInput';
 
 function EditForm(props) {
   const {
@@ -15,11 +24,35 @@ function EditForm(props) {
     onReset,
     submitButtonLabel,
   } = props;
-
   const formValues = formState.values;
   const formErrors = formState.errors;
 
   const [isSubmitting, setIsSubmitting] = useState(false);
+  const [equipments, setEquipments] = useState([])
+  const [equipmentIds, setEquipmentIds] = useState([])
+
+  useEffect(() => {
+    getEquipments()
+      .then(data => {
+        setEquipments(data.data['result'])
+      })
+      .catch(() => toast.error("مشکلی در گرفتن لیست قطعات رخ داده است."))
+
+      if(formValues?.equipments){
+        setEquipmentIds(formValues.equipments.map(x=>x.id))
+      }
+      console.log(formValues.equipments)
+  }, [])
+
+    useEffect(() => {
+
+      if(formValues?.equipments){
+        setEquipmentIds(formValues.equipments.map(x=>x.id))
+      }
+      console.log('setvalues',formValues.equipments)
+  }, [formValues])
+
+
 
   const handleSubmit = useCallback(
     async (event) => {
@@ -41,6 +74,20 @@ function EditForm(props) {
     }
   }, [formValues, onReset]);
 
+  // add equipment ids in array like :[1,2,3]
+  const handleEquipmentIds = (event) => {
+    const {
+      target: { value },
+    } = event;
+
+    console.log('handleEquipmentIds', value)
+
+    setEquipmentIds(
+      // On autofill we get a stringified value.
+      typeof value === 'string' ? value.split(',') : value,
+    );
+    onFieldChange("equipmentIds", value)
+  };
   return (
     <Box
       component="form"
@@ -63,7 +110,30 @@ function EditForm(props) {
               fullWidth
             />
           </Grid>
-         
+          <Grid size={{ xs: 12, sm: 6 }} sx={{ display: 'flex' }}>
+            <FormControl sx={{ m: 1, width: 300 }}>
+              <InputLabel id="demo-multiple-name-label">قطعات</InputLabel>
+              <Select
+                labelId="demo-multiple-name-label"
+                id="demo-multiple-name"
+                multiple
+                value={equipmentIds}
+                onChange={handleEquipmentIds}
+                input={<OutlinedInput label="قطعات" />}
+                fullWidth
+              >
+                {equipments.map((eq) => (
+                  <MenuItem
+                    style={{ margin: '5px' }}
+                    key={eq.id}
+                    value={eq.id}
+                  >
+                    {eq.name}
+                  </MenuItem>
+                ))}
+              </Select>
+            </FormControl>
+          </Grid>
         </Grid>
       </FormGroup>
       <Stack direction="row" spacing={2} justifyContent="space-between">
