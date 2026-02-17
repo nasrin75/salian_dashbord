e4f302f751e41c04d9d3016d4c12180328ffecd3
Nasrin Hasanpour <n75hasanpur@gmail.com>
2026-02-17 15:03:30 +0330

    delete inused func

--- src/utlis/constants/common.js
+++ src/utlis/constants/common.js
@@ -2,9 +2,3 @@
 export function StoreTokenInLocalStorage(token) {
     localStorage.setItem('token', token)
 }
-
-export function GetTokenFromLocalStorage() {
-    localStorage.getItem('token')
-}
-
-export const getToken = localStorage.getItem('token')
