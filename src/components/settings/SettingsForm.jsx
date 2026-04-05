4bcb075758aa43788d2c23fc8e1bbf2eebd72312
Nasrin Hasanpour <n75hasanpur@gmail.com>
2026-04-05 15:20:11 +0330

    setting page

---
+++ src/components/settings/SettingsForm.jsx
@@ -0,0 +1,120 @@
+import { useState, useCallback } from 'react';
+import PropTypes, { object } from 'prop-types';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import FormGroup from '@mui/material/FormGroup';
+import Grid from '@mui/material/Grid';
+import Stack from '@mui/material/Stack';
+import FormControl from '@mui/material/FormControl';
+import InputLabel from '@mui/material/InputLabel';
+import Select from '@mui/material/Select';
+import MenuItem from '@mui/material/MenuItem';
+
+function SettingsForm(props) {
+  const {
+    formState,
+    onFieldChange,
+    onSubmit,
+    onReset,
+    submitButtonLabel,
+  } = props;
+  const formValues = formState.values;
+  const formErrors = formState.errors;
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const activeSmsProvider = Object.values(formValues)
+    .find(item => item.key === 'active_sms_provider');
+
+  const handleSubmit = useCallback(
+    async (event) => {
+      event.preventDefault();
+
+      setIsSubmitting(true);
+      try {
+        await onSubmit(formValues);
+      } finally {
+        setIsSubmitting(false);
+      }
+    },
+    [formValues, onSubmit],
+  );
+
+  const handleReset = useCallback(() => {
+    if (onReset) {
+      onReset(formValues);
+    }
+  }, [formValues, onReset]);
+
+
+  return (
+    <Box
+      component="form"
+      onSubmit={handleSubmit}
+      noValidate
+      autoComplete="off"
+      onReset={handleReset}
+      sx={{ width: '100%' }}
+    >
+      <FormGroup>
+
+        <Grid container spacing={2} sx={{ mb: 2, width: '100%' }}>
+          <FormControl sx={{ m: 1, width: 300 }}>
+            <InputLabel id="active-sms-label">پنل پیامکی فعال</InputLabel>
+
+            <Select
+              labelId="active-sms-label"
+              id="active-sms-provider"
+              fullWidth
+              value={activeSmsProvider?.value ?? ''}
+              onChange={(e) => onFieldChange('active_sms_provider', e.target.value)}
+            >
+              <MenuItem key="kavenegar" value="kavenegar">
+                کاوه نگار
+              </MenuItem>
+
+              <MenuItem key="sabanovin" value="sabanovin" >
+                صبانوین
+              </MenuItem>
+            </Select>
+          </FormControl>
+
+
+        </Grid>
+      </FormGroup>
+      <Stack direction="row" spacing={2} justifyContent="space-between">
+        <Button
+          type="submit"
+          variant="contained"
+          size="large"
+          loading={isSubmitting}
+        >
+          {submitButtonLabel}
+        </Button>
+      </Stack>
+    </Box>
+  );
+}
+
+// SettingsForm.propTypes = {
+//   formState: PropTypes.shape({
+//     errors: PropTypes.shape({
+//       username: PropTypes.string,
+//       newPassword: PropTypes.string,
+//       email: PropTypes.string,
+//       mobile: PropTypes.string,
+//     }).isRequired,
+//     values: PropTypes.shape({
+//       username: PropTypes.string,
+//       newPassword: PropTypes.string,
+//       email: PropTypes.string,
+//       mobile: PropTypes.string,
+//     }).isRequired,
+//   }).isRequired,
+//   onFieldChange: PropTypes.func.isRequired,
+//   onSubmit: PropTypes.func.isRequired,
+//   submitButtonLabel: PropTypes.string.isRequired,
+// };
+
+
+export default SettingsForm;
+
---
+++ src/pages/settings/Settings.jsx
@@ -0,0 +1,186 @@
+import { useState, useCallback, useMemo, useEffect } from 'react';
+import Alert from '@mui/material/Alert';
+import Box from '@mui/material/Box';
+import CircularProgress from '@mui/material/CircularProgress';
+import EditForm from '../../components/profile/EditForm';
+import PageContainer from '../../components/PageContainer';
+import { toast } from 'react-toastify';
+import { EditValidation } from '../../validation/ProfileValidation';
+import Divider from '@mui/material/Divider';
+import { useNavigate, useParams } from 'react-router-dom';
+import { getDetails, updateProfile } from '../../api/ProfileApi';
+import { getSettings, updateSetting } from '../../api/SettingApi';
+import SettingsForm from '../../components/settings/SettingsForm';
+
+function SettingEditForm({ initialValues, onSubmit }) {
+    const navigate = useNavigate();
+
+    const [formState, setFormState] = useState(() => ({
+        values: initialValues,
+        errors: {},
+    }));
+    const formValues = formState.values;
+    const formErrors = formState.errors;
+
+    const setFormValues = useCallback((newFormValues) => {
+        setFormState((previousState) => ({
+            ...previousState,
+            values: newFormValues,
+        }));
+    }, []);
+
+    useEffect(() => {
+        if (initialValues) {
+            setFormState({
+                values: {
+                    ...initialValues,
+                },
+                errors: {}
+            })
+        }
+    }, [initialValues])
+
+    const setFormErrors = useCallback((newFormErrors) => {
+        setFormState((previousState) => ({
+            ...previousState,
+            errors: newFormErrors,
+        }));
+    }, []);
+
+    const handleFormFieldChange = useCallback(
+        (name, value) => {
+
+            const newItems = Object.values(formValues).map(item => {
+                if (item.key == name) {
+                    return { ...item, value }
+                }
+                return item;
+            })
+
+            const newFormValues = { ...newItems };
+
+            setFormValues(newFormValues);
+
+            const { issues } = EditValidation(newFormValues);
+
+            setFormErrors({
+                ...formErrors,
+                [name]: issues?.find(i => i.path?.[0] === name)?.message,
+            });
+
+        },
+        [formValues, setFormValues, formErrors],
+    );
+
+    const handleFormReset = useCallback(() => {
+        setFormValues(initialValues);
+    }, [initialValues, setFormValues]);
+
+    const handleFormSubmit = useCallback(async () => {
+
+        // const { issues } = EditValidation(formValues);
+
+        // if (issues && issues.length > 0) {
+        //     setFormErrors(
+        //         Object.fromEntries(issues.map((issue) => [issue.path?.[0], issue.message])),
+        //     );
+        //     return;
+        // }
+        // setFormErrors({});
+
+        try {
+            await onSubmit(formValues);
+            toast.success("ویرایش با موفقیت انجام شد.")
+
+        } catch (editError) {
+            toast.error("مشکلی در گرفتن اطلاعات رخ داده است")
+        }
+    }, [formValues, navigate, onSubmit, setFormErrors]);
+
+    return (
+        <SettingsForm
+            formState={formState}
+            onFieldChange={handleFormFieldChange}
+            onSubmit={handleFormSubmit}
+            onReset={handleFormReset}
+            submitButtonLabel="ذخیره"
+        />
+    );
+}
+
+export default function Setting() {
+    const [settings, setSettings] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    const loadData = useCallback(async () => {
+        setError(null);
+        setIsLoading(true);
+
+        getSettings()
+            .then(data => {
+                setSettings(data.data['result'])
+
+                setIsLoading(false);
+            })
+
+        setIsLoading(false);
+    }, [setSettings]);
+
+    useEffect(() => {
+        loadData();
+    }, [loadData]);
+
+    const handleSubmit = useCallback(
+
+        async (formValues) => {
+            updateSetting(formValues)
+                .then(data => {
+                    setSettings(data.data['result'])
+                    setIsLoading(false);
+                })
+        },
+        [settings],
+    );
+
+    const renderEdit = useMemo(() => {
+        if (isLoading) {
+            return (
+                <Box
+                    sx={{
+                        flex: 1,
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                        width: '100%',
+                        m: 1,
+                    }}
+                >
+                    <CircularProgress />
+                </Box>
+            );
+        }
+        if (error) {
+            return (
+                <Box sx={{ flexGrow: 1 }}>
+                    <Alert severity="error">{error.message}</Alert>
+                </Box>
+            );
+        }
+
+        return settings ? (
+            <SettingEditForm initialValues={settings} onSubmit={handleSubmit} />
+        ) : null;
+    }, [isLoading, error, settings, handleSubmit]);
+
+
+    return (
+        <PageContainer
+            title={"تنظیمات کلی"}
+        >
+            <Divider sx={{ marginBottom: "4%" }} />
+            <Box sx={{ display: 'flex', flex: 1 }}>{renderEdit}</Box>
+        </PageContainer>
+    );
+}
