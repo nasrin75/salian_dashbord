47448de0bfc4764403831eaf5b7d9cc05b96ae95
Nasrin Hasanpour <n75hasanpur@gmail.com>
2026-04-05 15:17:06 +0330

    fix fa char problem

--- src/pages/inventory/Create.jsx
+++ src/pages/inventory/Create.jsx
@@ -22,8 +22,6 @@ const INITIAL_FORM_VALUES = {
     Description: null,
     BrandName: '',
     ModelName: '',
-    Capacity: null,
-    Size: null,
     ExpireWarrantyDate: null,
     DeliveryDate: null,
     Features: []
@@ -58,13 +56,24 @@ export default function Create() {
     const handleFormFieldChange = React.useCallback(
         (name, value, type = "text") => {
             let finalValue = value;
+            if (type === 'number') {
+                finalValue = finalValue
+                    .replace(/([۰-۹])/g, (digit) => {
+                        const persianDigits = ['۰', '۱', '۲', '۳', '۴', '۵', '۶', '۷', '۸', '۹'];
+                        return persianDigits.indexOf(digit);
+                    })
+                    .replace(/[^0-9]/g, ''); //delete charachter is not english
 
-            if (type == 'switch') {
-                finalValue = value == 'on' ? true : false;
+                if (finalValue === '') {
+                    finalValue = null;
+                } else {
+                    finalValue = Number(finalValue);
+                    if (isNaN(finalValue)) {
+                        finalValue = null;
+                    }
+                }
             }
-            if (type == 'radio' || type == 'number') {
-                finalValue = Number(value);
-            }
+
             const newFormValues = {
                 ...formValues,
                 [name]: finalValue,
